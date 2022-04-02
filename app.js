fc69ecb5cc186ef5bf13b670c6c8d6c90f49d4c6
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-02 20:36:56 +0200

    added the logic and functions for the cards and the flipped cards and if its a match or not

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const randomize = () =>{
 const cardGenerator = () => {
     const cardData = randomize();
     //generate the html
+    
     cardData.forEach((item) => {
         const card = document.createElement("div");
         const face = document.createElement("img");
@@ -47,6 +48,8 @@ const cardGenerator = () => {
         back.classList = 'back';
         //attach the info to the cards
         face.src = item.imgSrc;
+        card.setAttribute('name', item.name);
+        
         //attach the cards to the section
         section.appendChild(card);
         card.appendChild(face);
@@ -55,10 +58,34 @@ const cardGenerator = () => {
 
         card.addEventListener('click', (e) => {
             card.classList.toggle("toggleCard");
+            checkCards(e);
         });
     });
 };
-
+//check cards
+const checkCards = (e) => {
+    console.log(e);
+    const clickedCard = e.target;
+    clickedCard.classList.add("flipped");
+    const flippedCards = document.querySelectorAll('.flipped');
+    console.log(flippedCards);
+    //logic
+    if(flippedCards.length === 2) {
+        if(flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')){
+            console.log("match");
+            flippedCards.forEach(card => {
+                card.classList.remove("flipped");
+                card.style.pointerEvents = 'none';
+            });
+        } else{
+            console.log("wrong");
+            flippedCards.forEach(card => {
+                card.classList.remove("flipped");
+                setTimeout(() => card.classList.remove("toggleCard"), 1000);
+            });
+        }
+    }
+};
 
 cardGenerator();
 
